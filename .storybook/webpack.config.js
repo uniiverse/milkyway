811f4847ef7f223531423dd5b6464a31fe7688fd
Kostia Arlouski <kostia.arlouski@universe.com>
2021-01-20 13:53:27 -0500

    Revert resolve

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,7 @@
 const path  = require('path');
 
 module.exports = async ({ config }) => {
-  config.resolve.extensions = [...config.resolve.extensions, '.css', '.svg'];
+  config.resolve.extensions.push('.svg');
 
   config.module.rules = config.module.rules.map(data => {
     if (/svg\|/.test( String( data.test ) )) data.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
