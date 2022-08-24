f375ed8023a84a94403ca95fac0c968eb88b7bb9
DAVID WOOD <davidnmwood@gmail.com>
2022-08-24 12:26:42 -0400

    DW: cleanup

--- stories/pamphlet/index.js
+++ stories/pamphlet/index.js
@@ -4,25 +4,16 @@ import { storiesOf } from '@storybook/react';
 import { array, text } from "@storybook/addon-knobs";
 
 import { Pamphlet } from '../../components/Pamphlet/Pamphlet';
+import {
+  title,
+  priceText,
+  description,
+  imageUrl,
+  upgradeOptions,
+  buttonText
+} from './preview-variables';
 
 const stories = storiesOf('Pamphlet', module);
-
-const title = "VIP ticket upgrade - Best experience"
-const priceText = "Only $40.00 extra per ticket"
-const description = "🎉 Enhance your experience at the event!"
-const imageUrl = "https://www.tastingtable.com/img/gallery/11-cocktails-to-try-if-you-like-drinking-gin/intro-1640186734.webp"
-const upgradeOptions = [
-  "Priority access",
-  "Flexibility to arrive +/- 1hr of your time",
-  "Limited edition poster",
-  "greet and meetLimited edition cushion",
-  "VIP souvenir laminate",
-  "Priority access",
-  "Flexibility to arrive +/- 1hr of your time",
-  "Free available T-shirt while products last",
-  "Special seating"
-]
-const buttonText = "UPGRADE ALL TICKETS"
 
 stories
   .add('Rate Upgrades', () => (
---
+++ stories/pamphlet/preview-variables.js
@@ -0,0 +1,16 @@
+export const title = "VIP ticket upgrade - Best experience"
+export const priceText = "Only $40.00 extra per ticket"
+export const description = "🎉 Enhance your experience at the event!"
+export const imageUrl = "https://www.tastingtable.com/img/gallery/11-cocktails-to-try-if-you-like-drinking-gin/intro-1640186734.webp"
+export const buttonText = "UPGRADE ALL TICKETS"
+export const upgradeOptions = [
+    "Priority access",
+    "Flexibility to arrive +/- 1hr of your time",
+    "Limited edition poster",
+    "greet and meetLimited edition cushion",
+    "VIP souvenir laminate",
+    "Priority access",
+    "Flexibility to arrive +/- 1hr of your time",
+    "Free available T-shirt while products last",
+    "Special seating"
+]
