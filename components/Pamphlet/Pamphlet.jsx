59aae21cf4a141bc2563ffc2df10e4842b3f88a3
Sully <sulaiman.omarmarouf@universe.com>
2022-09-15 08:30:21 -0700

    add border and boxshadow like design

--- components/Pamphlet/Pamphlet.jsx
+++ components/Pamphlet/Pamphlet.jsx
@@ -8,23 +8,21 @@ export const Pamphlet = props => {
   const { title, subtitle, description, imageUrl, children, buttonText, onButtonClick } = props;
 
   return (
-    <>
-      <Container className={styles.container}>
-        <Image className={styles.bannerImage} src={imageUrl} />
+    <Container className={styles.container}>
+      <Image className={styles.bannerImage} src={imageUrl} />
 
-        <div className={styles.containerBody}>
-          <Header as="h3" className={styles.title}>
-            {title}
-          </Header>
-          <p className={styles.subtitle}>{subtitle}</p>
-          <p className={styles.description}>{description}</p>
-          {children}
-          <Button primary className={styles.button} onClick={onButtonClick}>
-            {buttonText}
-          </Button>
-        </div>
-      </Container>
-    </>
+      <div className={styles.containerBody}>
+        <Header as="h3" className={styles.title}>
+          {title}
+        </Header>
+        <p className={styles.subtitle}>{subtitle}</p>
+        <p className={styles.description}>{description}</p>
+        {children}
+        <Button primary className={styles.button} onClick={onButtonClick}>
+          {buttonText}
+        </Button>
+      </div>
+    </Container>
   );
 };
 
